a74615c9195323a1c754d736959d05794c8d2fed
erin <erinh816@gmail.com>
2020-05-10 13:15:06 -0400

    set up mongodb

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose');
 const Joi = require('@hapi/joi');
 const genres = require('./routes/genres.js')
 const express = require('express');
 const app = express();
+
+mongoose
+	.connect('mongodb://localhost/vividmovie')
+	.then(() => console.log('connected'))
+	.catch((err) => console.error('cannot connect', err));
 
 app.use(express.json());
 app.use('/api/genres',genres);
